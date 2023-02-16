880d94447acd402899fbf6560d49333ff768da8d
abdullah <2agourss25@gmail.com>
2023-02-16 14:09:37 +0530

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,3 @@
 # build_files.sh
-echo " BUILD START "
-Python3.10.7 -m pip install -r requirements.txt
-python3.10.7 manage.py collectstatic --noinput --clear
-echo " BUILD END "
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
